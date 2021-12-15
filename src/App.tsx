0968409a1fbd90ebd3f5aae0a686533d4d149ca6
Saymon Tavares <saymon.tavares@satc.edu.br>
2021-12-15 16:46:13 -0300

    Footer url github

--- src/App.tsx
+++ src/App.tsx
@@ -100,7 +100,7 @@ export default function App() {
 
         <div className="w-auto mx-3 my-2 h-6 justify-between">
           <p className="font-mono text-xs text-center text-slate-300">
-            Developed by <b className="underline decoration-sky-300">Saymon Tavares</b>
+            Developed by <a href="https://github.com/saymontavares" target="_blank" className="underline decoration-sky-300">Saymon Tavares</a>
           </p>
         </div>
       </div>
